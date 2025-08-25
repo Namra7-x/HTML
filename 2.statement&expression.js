664d4845305ceb4d7bc9ed952e1ae3709b2a94c8
Namra7-x <patelnamra573@gmail.com>
2025-08-25 22:51:43 +0530

    Add files via upload

---
+++ 2.statement&expression.js
@@ -0,0 +1,5 @@
+//expression => code that produces a value example = 4 + 3 ans = 7
+
+//statement => code that performs action but doesn't return a value example => let name = "namra";
+
+//perform in console in expression it gives 7 while statement gives undefined bcz action performed.
