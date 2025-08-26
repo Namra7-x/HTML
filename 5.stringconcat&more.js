a48ae6919db6e4823cc3c45ee871e34a7dca94a9
Namra7-x <patelnamra573@gmail.com>
2025-08-26 23:57:25 +0530

    Add files via upload

---
+++ 5.stringconcat&more.js
@@ -0,0 +1,25 @@
+//concat
+
+let fname= "namra ";
+
+let lname= "patel";
+
+console.log(fname + lname);
+
+
+//type coercion
+
+// case1 "namra" + 20 = namra20
+
+// case2 "10" + 2 = 102  //bcz + performs both concat and addition thats why this happening
+
+// case 3 "10" * 40 = 400 
+
+// case 4 "namra" * 34 = Nan  // considerred as a failed operation
+
+//template literals
+
+let name = "namra";
+let age = 20;
+
+console.log(`i am ${name} and i am ${age} years old`);
