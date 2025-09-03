c44cc05fbf9e366033fad13e890e30d607d53133
Namra7-x <patelnamra573@gmail.com>
2025-09-03 22:22:55 +0530

    Add files via upload

---
+++ 10.typeof.js
@@ -0,0 +1,18 @@
+//typeof helps in error handling
+
+let age = 50;
+typeof age; // 'number'
+
+let name = 'namra';
+typeof name; //'string'
+
+let isAdmin = "true";
+typeof isAdmin; // 'boolean'
+
+
+//example for input field for the accepting mobile number look like this
+
+if(typeof moNumber !== "number"){
+    console.log("please enter a valid phone number");
+}
+
